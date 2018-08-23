3653a71274ca0c4f006d2cf84266623db3ad5448
jdey123 <james_dey@hotmail.com>
2018-08-23 15:26:33 +0100

    .

--- app/atmlocator.py
+++ app/atmlocator.py
@@ -49,7 +49,7 @@ def atmlocator(postcode, latitude, longitude):
 	Postcode:<input type="text" id="postcode" maxlength="8" size="8" name="postcode" value=\""""+str(postcode)+"""\">
     <input type="hidden" id="latitude" name="latitude" size="8" value="">	
 	<input type="hidden" id="longitude" name="longitude" size="8" value="">	
-	<input type="submit" id="mapSubmit" value="Submit">
+	<input type="submit" id="Submit" value="Submit">
     """
 
 	#Default the ATM locator to 1 mile - suitable for london
@@ -85,7 +85,7 @@ def atmlocator(postcode, latitude, longitude):
     } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
-	document.getElementById('mapSubmit').submit();
+	document.getElementById("Submit").submit();
 }
 
 function showPosition(position) {
